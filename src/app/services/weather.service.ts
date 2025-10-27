f50df860742ae181e821d89ff0f74cc113e9af9b
Camila Kfouri <kfouri.camila@gmail.com>
2025-10-27 10:12:51 +0000

    feat: integrate with BFF and remove direct API calls

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,12 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IWeatherData } from 'src/app/models/interfaces/weather-data.interface';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  private apiKey = '6b955509e7ba21ecc46671a90fbc50ab';
+
+  private readonly BFF_URL = environment.bffUrl;
+
+  // private apiKey = '6b955509e7ba21ecc46671a90fbc50ab';
 
   constructor(
     private http: HttpClient
@@ -15,7 +20,13 @@ export class WeatherService {
 
   getWeatherDataByCity(cityName: string): Observable<IWeatherData> { 
     return this.http.get<IWeatherData>(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.apiKey}&units=metric`
-    )
+      `${this.BFF_URL}/weather/current`,
+      {
+        params: {
+          city: cityName,
+          units: 'metric'
+        }
+      }
+    );
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    bffUrl: 'https://your-domain.com/api'
+};
